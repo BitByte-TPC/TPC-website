cfe1cbecaa678233be34339583f51ffee3bab52c
Aksh Bansal <akshmark@gmail.com>
2021-01-12 17:40:21 +0530

    mobile compatible

--- frontend/src/components/mainSection/index.jsx
+++ frontend/src/components/mainSection/index.jsx
@@ -5,7 +5,7 @@ function Main() {
   return (
     <div className={styles.container}>
         <span className={styles.heading}>The Programming Club IIITDMJ</span>
-        <img src="./ComponentTMP_0-image.png" />
+        <img src="./ComponentTMP_0-image.png" alt="The Programming Club"/>
         <span className={styles.text}>An Open Coding Community to explore and do a lot more with your friends and members.</span>
         <button className={styles.primaryBtn}>Explore Now</button>
     </div>
