06646af1977815fb5cd9bd44a2cbacfe2de36157
Aksh Bansal <akshmark@gmail.com>
2021-01-20 18:52:17 +0530

    about page added

---
+++ frontend/src/components/aboutUs/index.jsx
@@ -0,0 +1,42 @@
+import styles from "./style.module.css";
+
+function AboutUs() {
+  return (
+    <div id="about" className={styles.container}>
+      <div className={styles.content}>
+        <h1 className={styles.heading}>About &nbsp; Us</h1>
+        <p className={styles.para}>
+          Peeps at{" "}
+          <span className={styles.highlight}>
+            The Programming Club IIITDMJ{" "}
+          </span>
+          aim to help students and as a community we want to grow as much as
+          possible in many domains of coding and development, be it Competitive
+          Programming, Android, Web, Open Source, AI/ML, Ethical Hacking.
+        </p>
+        <p className={styles.para}>
+          We try to help each other learn new programming skills organize
+          sessions and competitions let everyone know of ongoing activities in
+          coding community and how they can use them for their benefits.
+        </p>
+        <p className={styles.para}>
+          Be it getting an internship in a startup company, cracking open source
+          competitions or to prepare for a product based company, someone will
+          always be there to guide you and help you with the resources to get
+          started.
+        </p>
+      </div>
+      <div className={styles.vectors}>
+        <div className={styles.svgCon}>
+          <img className={styles.aboutImg} src="./code2.png" alt="code" />
+          <img className={styles.aboutImg} src="./android.png" alt="android" />
+          <img className={styles.aboutImg} src="./gsoc.png" alt="gsoc" />
+          <img className={styles.aboutImg} src="./ai-ml.png" alt="ai-ml" />
+          <img className={styles.aboutImg} src="./hacker.png" alt="hacker" />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default AboutUs;
--- frontend/src/components/navLinks/index.jsx
+++ frontend/src/components/navLinks/index.jsx
@@ -1,26 +1,25 @@
-import styles from './style.module.css';
-import { Link } from 'react-router-dom';
-
+import styles from "./style.module.css";
+import { Link } from "react-router-dom";
 
 function NavLinks() {
   return (
     <>
-      <Link to='' className={styles.Link}>
+      <Link to="" className={styles.Link}>
         <li className={styles.first}>Club Registrations</li>
       </Link>
-      <Link to='' className={styles.Link}>
+      <Link to="" className={styles.Link}>
         <li className={styles.sec}>Leaderboard</li>
       </Link>
-      <Link to='' className={styles.Link}>
+      <Link to="" className={styles.Link}>
         <li className={styles.third}>Events</li>
       </Link>
-      <Link to='' className={styles.Link}>
+      <Link to="" className={styles.Link}>
         <li className={styles.fourth}>Sessions</li>
       </Link>
-      <Link to='' className={styles.Link}>
+      <Link to="" className={styles.Link}>
         <li className={styles.fifth}>Domains</li>
       </Link>
-      <Link to='' className={styles.Link}>
+      <Link to="" className={styles.Link}>
         <li className={styles.sixth}>Projects</li>
       </Link>
     </>
