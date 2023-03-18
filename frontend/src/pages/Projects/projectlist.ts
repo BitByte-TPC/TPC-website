19f990ae319ca33fcf733e827f3f0a4017da1d08
Tusharjain4 <jain0735@gmail.com>
2023-03-18 12:13:47 +0530

    projects added

--- frontend/src/pages/Projects/projectlist.ts
+++ frontend/src/pages/Projects/projectlist.ts
@@ -9,6 +9,24 @@ export const projectlist = [
     image: "/webix.iiitdmj.ac.in/images/projects/bitbot.png",
   },
   {
+    projectName: "Contest Saathi",
+    lang: "Javascript",
+    description:
+      "Contest Saathi allows enthusiasts of competitive programming to keep up with the latest competitions and get alerts about forthcoming ones.",
+    createdBy: "Tushar Jain",
+    url: "https://github.com/Tusharjain123/Contest-Saathi",
+    image: "/webix.iiitdmj.ac.in/images/projects/contestSaathi.png",
+  },
+  {
+    projectName: "Kairy",
+    lang: "Javascript",
+    description:
+      "Kairy is intended to simplify your online experience by organizing everything in one location, allowing you to be more productive.",
+    createdBy: "Prajjwal Kapoor",
+    url: "https://github.com/prajjwalkapoor/kairy",
+    image: "/webix.iiitdmj.ac.in/images/projects/kairy.png",
+  },
+  {
     projectName: "Jagrati Android App",
     lang: "Java",
     description: "Official AndroidApp of Jagrati - An Initiative of IIITians",
