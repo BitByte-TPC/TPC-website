ff4290908c5b06ece1df00b5f9a2caa5b17d252a
Tushar <103761473+Tusharjain123@users.noreply.github.com>
2023-03-19 08:37:51 +0530

    Team (#52)
    projects added

--- frontend/src/pages/Projects/projectlist.ts
+++ frontend/src/pages/Projects/projectlist.ts
@@ -9,6 +9,24 @@ export const projectlist = [
     image: "/webix.iiitdmj.ac.in/images/projects/bitbot.png",
   },
   {
+    projectName: "Contest Saathi",
+    lang: "Javascript",
+    description:
+      "Contest Saathi allows enthusiasts of competitive programming to keep up with the latest competitions and get alerts about forthcoming ones.",
+    createdBy: "Tushar Jain",
+    url: "https://github.com/Tusharjain123/Contest-Saathi",
+    image: "/webix.iiitdmj.ac.in/images/projects/contestSaathi.png",
+  },
+  {
+    projectName: "Kairy",
+    lang: "Javascript",
+    description:
+      "Kairy is intended to simplify your online experience by organizing everything in one location, allowing you to be more productive.",
+    createdBy: "Prajjwal Kapoor",
+    url: "https://github.com/prajjwalkapoor/kairy",
+    image: "/webix.iiitdmj.ac.in/images/projects/kairy.png",
+  },
+  {
     projectName: "Jagrati Android App",
     lang: "Java",
     description: "Official AndroidApp of Jagrati - An Initiative of IIITians",
@@ -32,6 +50,43 @@ export const projectlist = [
     createdBy: "Aksh Bansal",
     url: "https://micp-leaderboard.vercel.app/",
     image: "/webix.iiitdmj.ac.in/images/projects/micp.jpeg",
+  },
+  {
+    projectName: "Doc-AI",
+    lang: "Javascript",
+    description:
+      "This project is a real-time document editor built using React and Quill.js. It allows multiple users to collaborate on a document in real-time, similar to Google Docs. Also we have integrated the OpenAI so that user can give prompt to AI and get the results to write a documents.",
+    createdBy: "Vaishnav Deore, Priyansh Mehta, Tushit Shukla",
+    url: "https://github.com/Waishnav/Doc-AI",
+    image: "/webix.iiitdmj.ac.in/images/projects/docai.png",
+  },
+  {
+    projectName: "Expense Splitter",
+    lang: "Kotlin",
+    description:
+      "Expense Spliter is an android application built using Kotlin and Firebase that allows users to split expenses among friends.",
+    createdBy: "Priyanshu Agarwal, Pranjal Jha, Rishabh Gupta",
+    url: "https://github.com/IamRishabhGupta/expensesplitter",
+    image: "/webix.iiitdmj.ac.in/images/projects/expense.jpeg",
+  },
+  {
+    projectName: "News on Terminal",
+    lang: "Javascript",
+    description:
+      "News on terminal helps you to get news that you want right on your terminal so that you don't get bored while coding!",
+    createdBy: "Divyansh Tripathi, Prakash Dwivedi, Rameshwar Paryani",
+    url: "https://github.com/theoden42/news-on-terminal",
+    image: "/webix.iiitdmj.ac.in/images/projects/news.jpg",
+  },
+  {
+    projectName: "Splitit the Blockchain expense splitter",
+    lang: "Javascript",
+    description:
+      "We have built a split payments website where users can add friends and split payment among themselves. We have also added the function to settle payments using blockchain technology. ",
+    createdBy:
+      "Bhavik Agarwal, Deepanshu Singh, Samyak Bhargava, Raman Chaudhary",
+    url: "https://github.com/sdeepanshu17/hackbyte",
+    image: "/webix.iiitdmj.ac.in/images/projects/splitt.jpg",
   },
   {
     projectName: "Jagrati Wep App",
