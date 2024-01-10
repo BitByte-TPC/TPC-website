a46746d4934829870bf43852a3e817956d776227
Aryan <aryannewcollege@gmail.com>
2024-01-10 20:21:37 +0530

    Updated project data

--- frontend/src/pages/Projects/projectlist.ts
+++ frontend/src/pages/Projects/projectlist.ts
@@ -1,5 +1,77 @@
 export const projectlist = [
   {
+    projectName: "GetIt",
+    lang: "Nuxt, Django, Javascript",
+    description:
+      "Streamlined college e-commerce: Simplifying merch buying and selling for students.",
+    createdBy: "BitByte",
+    url: "https://github.com/bsoc-bitbyte/GetIt",
+    image: "/webix.iiitdmj.ac.in/images/projects/get_it.png",
+  },
+  {
+    projectName: "Fend",
+    lang: "Electron, TypeScript, React",
+    description:
+      "An SIH Winning Electron application with React and TypeScript that can be used to Harden Ubuntu based Operating Systems",
+    createdBy:
+      "Armin Patel, Prajjwal Kapoor, Varun Singh, Vansh Mittal, Aryan Pandey, Vaibhavi Bhosale",
+    url: "https://github.com/varun7singh/Fend",
+    image: "/webix.iiitdmj.ac.in/images/projects/fend.jpg",
+  },
+  {
+    projectName: "Doc-AI",
+    lang: "Javascript",
+    description:
+      "This project is a real-time document editor built using React and Quill.js. It allows multiple users to collaborate on a document in real-time, similar to Google Docs. Also we have integrated the OpenAI so that user can give prompt to AI and get the results to write a documents.",
+    createdBy: "Vaishnav Deore, Priyansh Mehta, Tushit Shukla",
+    url: "https://github.com/Waishnav/Doc-AI",
+    image: "/webix.iiitdmj.ac.in/images/projects/docai.png",
+  },
+  {
+    projectName: "Quix.ai",
+    lang: "TypeScript, Svelte, JavaScript, CSS",
+    description:
+      "Quix.ai is an intelligent VSCode extension powered by AI, designed to revolutionize your coding experience. It seamlessly adds a layer of brilliance to your workflow by automating code comments, summarization and documentation.",
+    createdBy: "Bhavik Agarwal",
+    url: "https://github.com/Bhavik-ag/Quix.ai",
+    image: "/webix.iiitdmj.ac.in/images/projects/quix.jpg",
+  },
+  {
+    projectName: "Kairy",
+    lang: "Javascript",
+    description:
+      "Kairy is intended to simplify your online experience by organizing everything in one location, allowing you to be more productive.",
+    createdBy: "Prajjwal Kapoor",
+    url: "https://github.com/prajjwalkapoor/kairy",
+    image: "/webix.iiitdmj.ac.in/images/projects/kairy.png",
+  },
+  {
+    projectName: "Visual Web Code",
+    lang: "Typescript",
+    description:
+      "Feature-rich code editor in your browser. Just upload your file or an image of code and start coding.",
+    createdBy: "Aksh Bansal",
+    url: "https://visualwebcode.netlify.app/",
+    image: "/webix.iiitdmj.ac.in/images/projects/visual-web-code.jpeg",
+  },
+  {
+    projectName: "Watcher",
+    lang: "Python, Shell",
+    description:
+      "Watcher is CLI-app (at this moment) which helps you to get perspective about your Screen-time",
+    createdBy: "Vaishnav Deore",
+    url: "https://github.com/Waishnav/Watcher",
+    image: "/webix.iiitdmj.ac.in/images/projects/watcher.png",
+  },
+  {
+    projectName: "MICP Leaderboard",
+    lang: "Typescript",
+    description: "Most Improved Competitive Programmers Leaderboard",
+    createdBy: "Aksh Bansal",
+    url: "https://micp-leaderboard.vercel.app/",
+    image: "/webix.iiitdmj.ac.in/images/projects/micp.jpeg",
+  },
+  {
     projectName: "BitBot",
     lang: "Typescript",
     description:
@@ -18,56 +90,12 @@ export const projectlist = [
     image: "/webix.iiitdmj.ac.in/images/projects/contestSaathi.png",
   },
   {
-    projectName: "Kairy",
-    lang: "Javascript",
-    description:
-      "Kairy is intended to simplify your online experience by organizing everything in one location, allowing you to be more productive.",
-    createdBy: "Prajjwal Kapoor",
-    url: "https://github.com/prajjwalkapoor/kairy",
-    image: "/webix.iiitdmj.ac.in/images/projects/kairy.png",
-  },
-  {
     projectName: "Jagrati Android App",
     lang: "Java",
     description: "Official AndroidApp of Jagrati - An Initiative of IIITians",
     createdBy: "BitByte",
     url: "https://github.com/BitByte-TPC/Jagrati",
     image: "/webix.iiitdmj.ac.in/images/projects/jagriti.jpg",
-  },
-  {
-    projectName: "Visual Web Code",
-    lang: "Typescript",
-    description:
-      "Feature-rich code editor in your browser. Just upload your file or an image of code and start coding.",
-    createdBy: "Aksh Bansal",
-    url: "https://visualwebcode.netlify.app/",
-    image: "/webix.iiitdmj.ac.in/images/projects/visual-web-code.jpeg",
-  },
-  {
-    projectName: "MICP Leaderboard",
-    lang: "Typescript",
-    description: "Most Improved Competitive Programmers Leaderboard",
-    createdBy: "Aksh Bansal",
-    url: "https://micp-leaderboard.vercel.app/",
-    image: "/webix.iiitdmj.ac.in/images/projects/micp.jpeg",
-  },
-  {
-    projectName: "Doc-AI",
-    lang: "Javascript",
-    description:
-      "This project is a real-time document editor built using React and Quill.js. It allows multiple users to collaborate on a document in real-time, similar to Google Docs. Also we have integrated the OpenAI so that user can give prompt to AI and get the results to write a documents.",
-    createdBy: "Vaishnav Deore, Priyansh Mehta, Tushit Shukla",
-    url: "https://github.com/Waishnav/Doc-AI",
-    image: "/webix.iiitdmj.ac.in/images/projects/docai.png",
-  },
-  {
-    projectName: "Expense Splitter",
-    lang: "Kotlin",
-    description:
-      "Expense Spliter is an android application built using Kotlin and Firebase that allows users to split expenses among friends.",
-    createdBy: "Priyanshu Agarwal, Pranjal Jha, Rishabh Gupta",
-    url: "https://github.com/IamRishabhGupta/expensesplitter",
-    image: "/webix.iiitdmj.ac.in/images/projects/expense.jpeg",
   },
   {
     projectName: "News on Terminal",
@@ -79,7 +107,7 @@ export const projectlist = [
     image: "/webix.iiitdmj.ac.in/images/projects/news.jpg",
   },
   {
-    projectName: "Splitit the Blockchain expense splitter",
+    projectName: "Splitit : The Blockchain expense splitter",
     lang: "Javascript",
     description:
       "We have built a split payments website where users can add friends and split payment among themselves. We have also added the function to settle payments using blockchain technology. ",
@@ -103,5 +131,14 @@ export const projectlist = [
     createdBy: "BitByte",
     url: "https://alumni.iiitdmj.ac.in/",
     image: "/webix.iiitdmj.ac.in/images/projects/alumini.jpeg",
+  },
+  {
+    projectName: "Expense Splitter",
+    lang: "Kotlin",
+    description:
+      "Expense Spliter is an android application built using Kotlin and Firebase that allows users to split expenses among friends.",
+    createdBy: "Priyanshu Agarwal, Pranjal Jha, Rishabh Gupta",
+    url: "https://github.com/IamRishabhGupta/expensesplitter",
+    image: "/webix.iiitdmj.ac.in/images/projects/expense.jpeg",
   },
 ];
